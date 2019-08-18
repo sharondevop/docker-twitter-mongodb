870303eb8d5c71f677e2de4aef1793283a14de9f
Sharon Mafgaoker <sharon.mafgaoker@vatbox.com>
2019-08-18 07:31:43 +0300

    Add variable script file

---
+++ set-var.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+### To use the Twitter API you need your personal consumer key,
+### consumer secret, access token and acces token secret.
+### You can get it from the https://developer.twitter.com/en/apps
+
+# Set your secret API and keys.
+export CONSUMER_KEY='xx'
+export CONSUMER_SECRET='xx'
+export ACCESS_TOKEN='xx'
+export ACCESS_TOKEN_SECRET='xx'
